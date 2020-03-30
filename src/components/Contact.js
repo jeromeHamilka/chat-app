fd3ab46115b57831d57bdf0522ae219d0c2bb98e
Jérôme HAMILKA <jerome.hamilka@gmail.com>
2020-03-30 16:22:03 +0200

    Modif vers quete 3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,29 @@
 import React from "react";
 import "./Contact.css";
+import PropTypes from "prop-types";
 
-const name = "Brad Gibson";
-const avatar = "https://randomuser.me/api/portraits/thumb/men/75.jpg";
-const online = true;
-
-function Contact() {
+function Contact(props) {
   return (
     <div className="Contact">
-      <img className="avatar" alt="user name" src={avatar}></img>
+      <img className="avatar" alt="user name" src={props.avatar} />
 
       <div>
-        <h4 className="name">{name}</h4>
+        <h4 className="name">{props.name}</h4>
         <div className="status">
-          <div className={online ? "status-online" : "status-offline"}></div>
-          <span className="status-text">{online ? "Online" : "Offline"}</span>
+          <div className={props.online ? "status-online" : "status-offline"} />
+          <span className="status-text">
+            {props.online ? "Online" : "Offline"}
+          </span>
         </div>
       </div>
     </div>
   );
 }
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  online: PropTypes.bool.isRequired
+};
 
 export default Contact;
